8d1a2271d2e2e150b5dc907541d5488762f2ce9a
Parochak <paromitachakma121@gmail.com>
2026-09-22 17:36:15 +0000

    Deploying to gh-pages from @ Parochak/Parochak.github.io@d5dbab74940dd31f77d8dfeb17814db438b1c0a3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -21,7 +21,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
+            window.location.href = "/coverletter/";
           },
         },{id: "nav-cv",
           title: "CV",
